8945c9919012d1fd32905a6ef62c4b6eaa6d3fc4
Uyen Trang <nnutrang19@clc.fitus.edu.vn>
2021-12-15 00:55:17 +0700

    ReadArticle 90%( displayYTvideo)

--- app/src/main/java/com/redhub/controller/mainscreen/ReadArticleActivity.kt
+++ app/src/main/java/com/redhub/controller/mainscreen/ReadArticleActivity.kt
@@ -1,11 +1,14 @@
 package com.redhub.controller.mainscreen
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.database.*
 import com.redhub.databinding.ActivityReadArticleBinding
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.redhub.model.DirectorModel
+import com.redhub.model.StarModel
 
 
 class ReadArticleActivity : AppCompatActivity() {
@@ -17,29 +20,38 @@ class ReadArticleActivity : AppCompatActivity() {
         binding = ActivityReadArticleBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val articleId: String = ""
+        database = FirebaseDatabase.getInstance().getReference("Article")
         readData(articleId)
+
     }
 
     private fun readData(articleId: String) {
-        database = FirebaseDatabase.getInstance().getReference("Article")
+
         database.child(articleId).get().addOnSuccessListener {
             if (it.exists()) {
                 val title = it.child("title").value
                 val description = it.child("description").value
                 val release_date = it.child("release_date").value
                 val gerne = it.child("gerne").value
-                val director = it.child("director").value
-                val star = it.child("star").value
                 val num_rate = it.child("num_rate").value
                 val rate = it.child("rate").value
                 val poster = it.child("posterUri").value
                 val youtubeID = it.child("youtubeID").value
-                val review = it.child("review").value
+                //val review = it.child("review").value
+
+
 
                 Toast.makeText(this, "Successfuly Read", Toast.LENGTH_SHORT).show()
-                //binding.etusername.text.clear()
-                binding.movieTitle.text = title.toString()
 
+                binding.movieTitle.text = title.toString()
+                binding.movieDescription.text = description.toString()
+                binding.movieReleaseDate.text= release_date.toString()
+                binding.movieGerne.text = gerne.toString()
+                binding.movieNumRate.text=num_rate.toString()
+                binding.movieRating.text = rate.toString()
+                binding.ivMoviePoster.setImageURI(Uri.parse(poster.toString()))
+                readDirectorsList()
+                readStarList()
 
             }
         }.addOnFailureListener {
@@ -47,5 +59,54 @@ class ReadArticleActivity : AppCompatActivity() {
             Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    private fun readDirectorsList()
+    {
+        database.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list_directors = ArrayList<DirectorModel>()
+                for (director in snapshot.child("director").children)
+                {
+                    val model = director.getValue(DirectorModel::class.java)
+                    list_directors.add(model as DirectorModel)
+                }
+                if(list_directors.size >0)
+                {
+                    val adapter = DirectorAdapter(list_directors)
+                    binding.recyclerviewDirector.adapter = adapter
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Cancel",error.toString())
+            }
+
+        })
+    }
+
+    private fun readStarList()
+    {
+        database.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list_stars = ArrayList<StarModel>()
+                for (star in snapshot.child("star").children)
+                {
+                    val model = star.getValue(StarModel::class.java)
+                    list_stars.add(model as StarModel)
+                }
+                if(list_stars.size >0)
+                {
+                    val adapter = StarAdapter(list_stars)
+                    binding.recyclerviewStar.adapter = adapter
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Cancel",error.toString())
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/redhub/controller/mainscreen/SearchAdapter.kt
@@ -0,0 +1,38 @@
+package com.redhub.controller.mainscreen
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.redhub.R
+import com.redhub.model.ArticleModel
+import com.redhub.model.StarModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.layout_search_list.view.*
+import kotlinx.android.synthetic.main.layout_star_list.view.*
+
+class SearchAdapter( private val list_search: ArrayList<ArticleModel>) : RecyclerView.Adapter<SearchAdapter.ViewHolder>(){
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    {
+        val title = itemView.movie_title
+        val rate = itemView.movie_rate
+        val imguri : ImageView=itemView.ArticleImageView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_search_list,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = list_search[position].title
+        holder.rate.text = list_search[position].rates.toString()
+        Picasso.get()
+            .load(list_search[position].posterUri)
+            .into(holder.imguri)
+    }
+
+    override fun getItemCount(): Int {
+        return list_search.size
+    }
+}
